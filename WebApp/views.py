4f756abbb7c9a5a23e7cfd698b4f136bb3ef6d44
Atlantox <atlantox7@gmail.com>
2022-11-09 17:32:49 -0400

    adding nav-icon

--- WebApp/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def home(request):
-    ctx = {}
-    return render(request, 'WebApp/home.html', ctx)
-
-def contact(request):
-    return render(request, 'WebApp/contact.html', dict())
